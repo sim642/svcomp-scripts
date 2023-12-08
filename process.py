d565537e38954287b5421ad6baa4aea9c66e3255
Simmo Saan <simmo.saan@gmail.com>
2023-12-08 11:09:23 +0200

    Use Counter for processing

--- process.py
+++ process.py
@@ -1,6 +1,7 @@
 from lxml import etree
 import bz2
 from pathlib import Path
+import collections
 
 def load_run_results(filename):
     with bz2.open(filename) as f:
@@ -17,22 +18,30 @@ def load_run_results(filename):
 
 def load_tool_results(tool):
     ret = {}
-    for file in Path(".").glob(f"{tool}.*.results.*.xml.bz2"):
+    for file in Path("data0/").glob(f"{tool}.*.results.*.xml.bz2"):
         ret |= load_run_results(file)
     return ret
 
-def ratio(verifier_results, validator_results):
-    verifier_trues = 0
-    verifier_trues_validated = 0
+def get_result_pairs(verifier_results, validator_results):
+    result_pairs = collections.Counter()
     for task, verifier_status in verifier_results.items():
+        if task in validator_results:
+            validator_status = validator_results[task]
+        else:
+            validator_status = None
+
+        result_pairs[(verifier_status, validator_status)] += 1
+
+    return result_pairs
+
+def ratio(result_pairs):
+    verifier_trues = 0
+    for result_pair, count in result_pairs.items():
+        verifier_status, validator_status = result_pair
         if verifier_status == "true":
-            verifier_trues += 1
+            verifier_trues += count
 
-            if task in validator_results:
-                validator_status = validator_results[task]
-                if validator_status == "true":
-                    verifier_trues_validated += 1
-
+    verifier_trues_validated = result_pairs[("true", "true")]
     return (verifier_trues_validated, verifier_trues)
 
 verifiers = ["goblint", "mopsa", "uautomizer", "cpachecker"]
@@ -44,5 +53,7 @@ for verifier in verifiers:
     for validator in validators:
         print(f"  {validator}")
         validator_results = load_tool_results(f"{validator}-validate-correctness-witnesses-2.0-{verifier}")
-        verifier_trues_validated, verifier_trues = ratio(verifier_results, validator_results)
+
+        result_pairs = get_result_pairs(verifier_results, validator_results)
+        verifier_trues_validated, verifier_trues = ratio(result_pairs)
         print(f"    {verifier_trues_validated}/{verifier_trues} = {verifier_trues_validated / verifier_trues * 100:.2f}%")
