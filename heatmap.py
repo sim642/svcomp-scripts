ef22aa5c914b209152bbe453c3ed51e22d75591e
Simmo Saan <simmo.saan@gmail.com>
2023-12-08 15:19:34 +0200

    Rewrite heatmap to separately compute true and false ratios

--- heatmap.py
+++ heatmap.py
@@ -1,21 +1,36 @@
 import pandas as pd
 import seaborn as sns
 
-data = pd.read_csv("out.csv")
-# data = pd.read_csv("out.csv", index_col=["verifier", "validator", "expected_verdict", "verifier_status", "validator_status"])
+data = pd.read_csv("out2.csv")
 print(data)
 
-correct = data[data["verifier_status"] == data["expected_verdict"]]
-correct_valid = correct[(correct["validator_status"] == correct["expected_verdict"]) | (correct["validator_status"] == "done")]
-print(correct)
-print(correct_valid)
+true = data[data["expected_verdict"] == "true"]
+false = data[data["expected_verdict"] == "false"]
 
-correct2 = correct[["verifier", "validator", "count"]].groupby(["verifier", "validator"]).agg("sum")
-correct_valid2 = correct_valid[["verifier", "validator", "count"]].groupby(["verifier", "validator"]).agg("sum")
+def is_false(s):
+    return s.startswith("false")
 
-ratio = correct_valid2 / correct2
-ratio = ratio.pivot_table(index="verifier", columns="validator", values="count")
-print(ratio)
+true_correct = true[true["verifier_status"] == "true"]
+false_correct = false[false["verifier_status"].map(is_false, na_action="ignore")]
 
-h = sns.heatmap(data=ratio, vmin=0.0, vmax=1.0, square=True, cmap="RdYlGn", xticklabels=True, yticklabels=True)
+true_valid = true_correct[(true_correct["validator_status"] == "true") | (true_correct["validator_status"] == "done")]
+false_valid = false_correct[false_correct["validator_status"].map(is_false, na_action="ignore") | (false_correct["validator_status"] == "done")]
+
+true_correct_group = true_correct[["verifier", "validator", "count"]].groupby(["verifier", "validator"]).agg("sum")
+false_correct_group = false_correct[["verifier", "validator", "count"]].groupby(["verifier", "validator"]).agg("sum")
+true_valid_group = true_valid[["verifier", "validator", "count"]].groupby(["verifier", "validator"]).agg("sum")
+false_valid_group = false_valid[["verifier", "validator", "count"]].groupby(["verifier", "validator"]).agg("sum")
+
+true_ratio = true_valid_group / true_correct_group
+false_ratio = false_valid_group / false_correct_group
+
+print(true_ratio)
+print(false_ratio)
+
+true_pivot = true_ratio.pivot_table(index="verifier", columns="validator", values="count").filter(axis=1, like="correctness") # TODO: some violation validators confirm trues?
+false_pivot = false_ratio.pivot_table(index="verifier", columns="validator", values="count").filter(axis=1, like="violation") # TODO: some correctness validators confirm falses?
+pivot = true_pivot.join(false_pivot, how="outer").sort_index(axis=1)
+
+h = sns.heatmap(data=pivot, vmin=0.0, vmax=1.0, square=True, cmap="RdYlGn", xticklabels=True, yticklabels=True)
 h.get_figure().savefig("out.svg", bbox_inches="tight")
+
