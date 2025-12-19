be5fbb1afe180246594caef3dac37abfb3647a86
Simmo Saan <simmo.saan@gmail.com>
2025-12-19 17:43:46 +0200

    Exclude unconfirmed results from portfolio-stats.py

--- goblint-svcomp2026/portfolio-stats.py
+++ goblint-svcomp2026/portfolio-stats.py
@@ -8,9 +8,8 @@
 import pandas as pd
 
 data = pd.read_csv("table-generator-level.table.csv", skiprows=2, sep="\t")
-# TODO: this also counts unconfirmed!
-print("true:")
-data_true = data[data["status"] == "true"]
+print("confirmed true:")
+data_true = data[(data["status"] == "true") & (data["category"] == "correct")]
 data_levels = data_true["level"].groupby(data["level"]).count().rename("count")
 data_cumlevels = data_levels.cumsum().rename("cumcount")
 data_levels_pct = (data_levels / data_levels.sum() * 100).rename("pct")
@@ -30,7 +29,6 @@ data_ro_levels_pct = (data_ro_levels / data_ro_levels.sum() * 100).rename("pct")
 print(pd.concat([data_ro_cumlevels, data_ro_levels, data_ro_levels_pct], axis=1))
 
 
-# TODO: this also counts unconfirmed!
 print("Overall score:")
 data_score = data #[data["status"] == "true"]
 # print(data_score)
@@ -39,7 +37,7 @@ weights = weights[weights["overallweight"].notnull()]
 # print(weights)
 data_weights = pd.merge(data_score, weights, how="left", left_on=["sv-benchmarks/c/", "Unnamed: 1"], right_on=["ymlfile", "property"], validate="1:1")
 
-data_weights_true = data_weights[data_weights["status"] == "true"]
+data_weights_true = data_weights[(data_weights["status"] == "true") & (data_weights["category_x"] == "correct")]
 data_weights_true["overall_score"] = data_weights_true["overallweight"] * 2 # assuming only trues
 # print(data_weights_true)
 data_weights_levels = data_weights_true.groupby(data_weights_true["level"])["overall_score"].sum()
@@ -49,8 +47,8 @@ data_weights_levels_pct = (data_weights_levels / data_weights_levels.sum() * 100
 print(pd.concat([data_weights_cumlevels, data_weights_levels, data_weights_levels_pct], axis=1))
 
 
-# TODO: this also counts unconfirmed! (so doesn't match official table)
 data_weights_true["meta_score"] = data_weights_true["weight"] * 2 # assuming only trues
-data_weights_meta = data_weights_true.groupby(data_weights_true["metacategory"])["meta_score"].sum()
+# print(data_weights_true)
+data_weights_meta = data_weights_true[data_weights_true["category_x"] == "correct"].groupby(data_weights_true["metacategory"])["meta_score"].sum().round()
 print(data_weights_meta)
 
