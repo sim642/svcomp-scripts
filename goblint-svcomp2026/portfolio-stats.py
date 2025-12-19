bd655c24a9440f83eddcd700f71d694505c7b389
Simmo Saan <simmo.saan@gmail.com>
2025-12-19 12:06:31 +0200

    Add Overall score to portfolio-stats.py

--- goblint-svcomp2026/portfolio-stats.py
+++ goblint-svcomp2026/portfolio-stats.py
@@ -1,7 +1,7 @@
 # /// script
 # requires-python = ">=3.12"
 # dependencies = [
-#     "pandas",
+#     "pandas==3.0.0rc0", # this version tells why merge validation fails
 # ]
 # ///
 
@@ -28,3 +28,22 @@ data_ro_levels = data_ro["level-started"].groupby(data["level-started"]).count()
 data_ro_cumlevels = data_ro_levels.cumsum().rename("cumcount")
 data_ro_levels_pct = (data_ro_levels / data_ro_levels.sum() * 100).rename("pct")
 print(pd.concat([data_ro_cumlevels, data_ro_levels, data_ro_levels_pct], axis=1))
+
+
+# TODO: this also counts unconfirmed!
+print("Overall score:")
+data_score = data #[data["status"] == "true"]
+# print(data_score)
+weights = pd.read_csv("weightstable.csv")
+weights = weights[weights["overallweight"].notnull()]
+# print(weights)
+data_weights = pd.merge(data_score, weights, how="left", left_on=["sv-benchmarks/c/", "Unnamed: 1"], right_on=["ymlfile", "property"], validate="1:1")
+
+data_weights_true = data_weights[data_weights["status"] == "true"]
+data_weights_true["overall_score"] = data_weights_true["overallweight"] * 2 # assuming only trues
+# print(data_weights_true)
+data_weights_levels = data_weights_true.groupby(data_weights_true["level"])["overall_score"].sum()
+data_weights_cumlevels = data_weights_levels.cumsum().rename("cumsum")
+data_weights_levels_pct = (data_weights_levels / data_weights_levels.sum() * 100).rename("pct")
+print(pd.concat([data_weights_cumlevels, data_weights_levels, data_weights_levels_pct], axis=1))
+
