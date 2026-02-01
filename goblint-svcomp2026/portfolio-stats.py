fa58e1d6b1e0bb1af0ebb2ab5aa1e1c76f3a17cc
Simmo Saan <simmo.saan@gmail.com>
2026-02-01 09:56:19 +0200

    Exclude invalid tasks from portfolio-stats

--- goblint-svcomp2026/portfolio-stats.py
+++ goblint-svcomp2026/portfolio-stats.py
@@ -8,6 +8,23 @@
 import pandas as pd
 
 data = pd.read_csv("table-generator-level.table.csv", skiprows=2, sep="\t")
+
+# TODO: deduplicate this with computeweights
+#open and read the exclude file
+svbenchpath = "/home/simmo/dev/goblint/sv-comp/sv-benchmarks"
+excluded = set()
+with open(svbenchpath + "/Invalid-TaskDefs.set", 'r') as f:
+    content=f.read()
+    for line in content.splitlines():
+        # ignore comments and empty lines
+        line=line.strip()
+        if line.startswith("#") or line == "":
+            continue
+        excluded.add(line.removeprefix("c/"))
+
+# exclude invalid tasks
+data.loc[data["sv-benchmarks/c/"].isin(excluded), "category"] = "missing" # like in fixed BenchExec results xml-s, they aren't adapted to invalid task for some reason, only HTMLs
+
 print("confirmed true:")
 data_true = data[(data["status"] == "true") & (data["category"] == "correct")]
 data_levels = data_true["level"].groupby(data["level"]).count().rename("count")
@@ -22,7 +39,7 @@ def is_ro(s):
     return s.startswith("TIMEOUT") or s.startswith("OUT OF MEMORY")
 
 print("resource out (TIMEOUT, OUT OF MEMORY):")
-data_ro = data[data["status"].map(is_ro)]
+data_ro = data[data["status"].map(is_ro) & (data["category"] != "missing")]
 data_ro_levels = data_ro["level-started"].groupby(data["level-started"]).count().rename("count")
 data_ro_cumlevels = data_ro_levels.cumsum().rename("cumcount")
 data_ro_levels_pct = (data_ro_levels / data_ro_levels.sum() * 100).rename("pct")
@@ -36,6 +53,8 @@ weights = pd.read_csv("weightstable.csv")
 weights = weights[weights["overallweight"].notnull()]
 # print(weights)
 data_weights = pd.merge(data_score, weights, how="left", left_on=["sv-benchmarks/c/", "Unnamed: 1"], right_on=["ymlfile", "property"], validate="1:1")
+
+# data_weights.to_csv("data-weights.csv")
 
 data_weights_true = data_weights[(data_weights["status"] == "true") & (data_weights["category_x"] == "correct")]
 data_weights_true["overall_score"] = data_weights_true["overallweight"] * 2 # assuming only trues
