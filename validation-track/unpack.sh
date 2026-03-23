6bb212d409f5618a8402b248ce0b2b869861b962
Simmo Saan <simmo.saan@gmail.com>
2026-03-23 11:44:24 +0200

    Unpack SV-COMP 2026 YAML witnesses

--- validation-track/unpack.sh
+++ validation-track/unpack.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+# Unpacks YAML witnesses.
+
 # https://unix.stackexchange.com/questions/168807/mount-zip-file-as-a-read-only-filesystem:
 # * fuse-zip
 # * archivemount
 # * fuse-archive
 # * ratarmount
 
-mkdir svcomp25-witnesses
-fuse-zip -r svcomp25-witnesses.zip svcomp25-witnesses
+mkdir svcomp26-witnesses
+fuse-zip -r svcomp26-witnesses.zip svcomp26-witnesses
 
-unzip svcomp25-results.zip 'results-verified/*fileHashes.json.gz'
+unzip svcomp26-results.zip 'results-verified/*fileHashes.json.gz'
 
 ./unpackFileStoresYamlWitness.py -o yaml-witnesses results-verified/*fileHashes.json.gz
+
+umount svcomp26-witnesses
